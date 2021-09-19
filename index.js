99949171882f1af98e7ad1949ff27c797714455d
Giancarlo <giancarlo.uzzo@community.unipa.it>
2021-09-19 17:41:01 +0200

    express con html file render

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.sendFile("home.html", { root: __dirname + "/public" });
 });
 
 app.all("*", (req, res) => {
-  res.send("Risorsa non trovata");
+  res.sendFile("404.html", { root: __dirname + "/public" });
 });
 
 app.listen(3000, () => console.log(`Server running`));
